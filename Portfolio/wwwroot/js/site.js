b3cc78e00f99bb1d60d814c229b8f16332eb42db
Egzon Bllaca <egzonbllaca88@gmail.com>
2022-07-19 12:47:34 +0200

    Added CV download

--- Portfolio/wwwroot/js/site.js
+++ Portfolio/wwwroot/js/site.js
@@ -7,4 +7,19 @@
         $('#menu').removeClass('fa-times');
         $('header').removeClass('toggle');
     });
+
+    //Smooth scrolling
+
+    $('a[href*="#"]').on('click', function (e) {
+
+        e.preventDefault();
+
+        $('html,body').animate({
+            scrollTop: $($(this).attr('href')).offset().top,
+
+        },
+            500,
+            'linear'
+        );
+    });
 });
